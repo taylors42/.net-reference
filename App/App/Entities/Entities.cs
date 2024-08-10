2aea4f2bc3e5172ff73dd387712e10ded2b8e973
Taylor <96550502+taylors42@users.noreply.github.com>
2024-08-10 15:27:20 -0300

    .

--- App/App/Entities/Entities.cs
+++ App/App/Entities/Entities.cs
@@ -1,18 +1,22 @@
-using System.Security.Cryptography.X509Certificates;
-
 namespace App.Entities
 {
     class Music
     {
-        public Music(Band BandName) // thats a constructor
+        public Music(Band SelfBand, string SelfName) // thats a constructor
         {
-            Name = BandName.Name;
+            Artist = SelfBand.Name;
+            Name = SelfName;
         }
-        public string? Name { get; set; }
-        private string? Artist { get; set; }
+
+        public string? Name { get; }
+        public string? Artist { get; }
         private bool Available { get; set; }
         public int? Duration { get; set; }
-        // public string? Slogan => $"Thats a slogan! of the song: {Name} by the artist: {Artist}";
+
+        public void Slogan()
+        {
+            Console.WriteLine($"Thats a slogan! of the song: {Name} by the artist: {Artist}");
+        }
 
         public void ShowDetails()
         {
@@ -21,42 +25,40 @@ namespace App.Entities
             Console.WriteLine($"Available: {(Available == true ? "Yes" : "No")}");
             Console.WriteLine($"Duration: {Duration}");
         }
+
         public void SetAvailable(bool Value)
         {
             Available = Value;
         }
-        
+
         public bool GetAvailable()
         {
             return Available;
-        }
-        
-        public void SetArtist(Band BandName)
-        {
-            this.Artist = BandName.Name;
-        }
-
-        public string? GetArtist()
-        {
-            return this.Artist;
         }
     }
 
     class Album
     {
+        public Album(string Self)
+        {
+            Name = Self;
+        }
+
         private List<Music> MusicList = new List<Music>();
-        public string? Name { get; set; }
+        public string Name { get; }
         public int TotalDuration => MusicList.Sum(x => x.Duration ?? 0);
+
         public void AddMusic(Music Self)
         {
             MusicList.Add(Self);
         }
+
         public void ShowMusics()
         {
             Console.WriteLine("List of musics: \n");
             foreach (Music song in MusicList)
             {
-                Console.WriteLine($"Song: {song.Name} - {song.GetArtist()}");
+                Console.WriteLine($"Song: {song.Name} - {song.Artist}");
             }
             Console.WriteLine("\nTotal duration of the album: " + TotalDuration);
         }
@@ -64,8 +66,15 @@ namespace App.Entities
 
     class Band
     {
+        public Band(string Self)
+        {
+            Name = Self;
+        }
+
         private List<Album> AlbumList = new List<Album>();
-        public string Name { get; set; }
+
+        public string? Name { get; }
+
         public void AddAlbum(Album album)
         {
             AlbumList.Add(album);
@@ -83,5 +92,39 @@ namespace App.Entities
     class CodeMath
     {
         public int Add(int a, int b) => a + b; // Thats a example of a lambda function
-    }  
+    }
+    
+    class Podcast
+    {
+        public Podcast(string PodcastName, string PodcastHost)
+        {
+            Name = PodcastName;
+            Host = PodcastHost;
+        }
+
+        public string? Name { get; }
+        public string? Host { get; }
+        public int TotalEpisodes()
+        {
+            return 
+        }
+        
+    }
+    
+    class EpisodePodcast
+    {
+        private List<string> Members = new List<string>();
+        public EpisodePodcast(string? Title, float Duration, string Podcast)
+        {
+            Title = Title;
+            Duration = Duration;
+        }
+        
+        public void AddMembers(string Member)
+        {
+            Members.Add(Member);
+        }
+        public string Title { get;  }
+        public float Duration { get;  }
+    }
 }
